12d057370a6228e85598c902f60c7e71f9c3a129
David Awad <Davidawad64@gmail.com>
2015-09-01 21:17:52 -0400

    fix api call for quote of day

--- server.py
+++ server.py
@@ -18,36 +18,28 @@ def home():
 
 def send_quote():
     r = requests.get('http://api.theysaidso.com/qod.json')
-    print r.text
-    print r.text.contents['quote']
-    print r.text.contents['author']
-
+    
+    # decompose JSON for quote
+    quote = str(r.json()['contents']['quotes'][0]['quote'])
+    # decompose JSON for author
+    author = str(r.json()['contents']['quotes'][0]['author'])
+    
 
     # Twilio sends the user a text message thanking them.
     # twil_client.messages.create( to="+1 732-925-4874", from_="+1 732-479-1418", body="#powerthrough" )
     twil_client.messages.create(
         to="+1 732-925-4874", # that's my number.
         from_="+1 732-479-1418", # change this number to your twilio trial number
-        body= ' "{0}" " - {1}'.format(r.text.contents['quote'], r.text.contents['author']) #this would then be the quote
+        body= ' "{0}" " - {1}'.format(quote, author) #this would then be the quote
         )
     return
 
-
+'''
 if __name__ == '__main__':
     app.run(
         debug=True
         )
+'''
 
-
-
-
-
-utc = pytz.utc
-loc_dt = utc.localize(datetime.datetime.today().replace(hour=9, minute=0))
-
-today = utc.localize(datetime.datetime.today())
-
-# if it's 9 am send our text. 
-if loc_dt < today:
-    print("sending quote")
+if __name__ == '__main__':
     send_quote()
